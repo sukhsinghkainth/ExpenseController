11624a83c3624f5bfa87a4c2d7306d19129d56d0
sukh <sukhsinghkainth@gmail.com>
2024-01-16 16:30:04 +0530

    settlement response add

---
+++ src/response/settlement.ts
@@ -0,0 +1,12 @@
+export class settlement {
+    constructor
+    (
+    public settlement : string,
+    public _id :  Number,
+    public amount : number,
+    public data : number,
+    public user : string,
+    public status: string 
+    ){
+    }
+}
