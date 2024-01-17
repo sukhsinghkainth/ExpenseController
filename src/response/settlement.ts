bc3acfd65c724e1c8d629324fec2e7ff16cc78cd
sukh <sukhsinghkainth@gmail.com>
2024-01-17 11:48:53 +0530

    changes in settlement add this keyword

--- src/response/settlement.ts
+++ src/response/settlement.ts
@@ -1,11 +1,23 @@
 export class settlement {
+    _id: Number;
+    amount: number;
+    data: number;
+    user: string;
+    status: string
     constructor
-    (
-    public _id :  Number,
-    public amount : number,
-    public data : number,
-    public user : string,
-    public status: string 
-    ){
+        (
+            _id: Number,
+            amount: number,
+            data: number,
+            user: string,
+            status: string
+        ) {
+        this._id = _id;
+        this.amount = amount;
+        this.data = data;
+        this.status = status;
+        this.user = user
+
+
     }
 }
