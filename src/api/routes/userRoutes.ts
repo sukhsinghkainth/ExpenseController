a336059ececf827c9a2dca20932bcc512f2dd0b0
sukh <sukhsinghkainth@gmail.com>
2024-01-23 14:50:34 +0530

    delete unwanted route

--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -1,17 +1,13 @@
 // user.routes.ts
 import express, { Request, Response } from 'express';
-import  addSampleData  from "../../services/expenService";
-import getuser from "../../services/getalldata"
 import madeExpense from "../../services/expenses"
-
 import createuser from "../../services/createuserService"
 
 const router = express.Router();
 
 // Add a route to add sample data
-router.get('/addSampleData', addSampleData);
+
 router.get("/expense/:username",madeExpense)
-router.get("/user/:userId",getuser)
 router.get("/createuser",createuser)
 
 
