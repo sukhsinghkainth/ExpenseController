3d459c0b8fdceae3f27815e0f8192c21de61568f
sukh <sukhsinghkainth@gmail.com>
2024-02-28 10:56:42 +0530

    Update - Added cors middleware to app.ts.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import config from './config/config';
 import { db } from './loaders/dbConnect';
 import userRoutes from './api/routes/userRoutes';
-
+import cors from 'cors';
 import categoryRoutes from "./api/routes/categoryRoutes"
 import cookieParser from "cookie-parser";
 import isAuth from './api/routes/middlewares/isAuth';
@@ -11,11 +11,16 @@ import incomeRoute from "./api/routes/transactionRoutes"
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true, // if your frontend sends credentials (e.g., cookies)
+}));
+
 
 app.use(cookieParser());
 
 app.use('/api/v1', userRoutes);
-app.use('/api/v1', isAuth, categoryRoutes);
+app.use('/api/v1',isAuth,categoryRoutes);
 app.use('/api/v1', isAuth, budgetRoutes);
 app.use("/api/v1", isAuth, incomeRoute)
 
